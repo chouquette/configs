56dbb3e79cac760370b5cc423fb9ac444088adf7
Jonathan Calmels <jbjcalmels@gmail.com>
2014-03-06 00:57:54 +0100

    Update setup script

--- setup.sh
+++ setup.sh
@@ -3,8 +3,13 @@
 SCRIPT_NAME=$(basename $0)
 SCRIPT_PATH=$(dirname $0)
 
+cd $SCRIPT_PATH
+git submodule init
+git submodule update
+cd -
+
 for file in $(ls -A "$SCRIPT_PATH"); do
-    if [ "$file" != "$SCRIPT_NAME" ] && [ "$file" != ".git" ]; then
+    if [ "$file" != "$SCRIPT_NAME" ] && [ "$file" != ".git" ] && [ "$file" != ".gitmodules" ]; then
         ln -s "$SCRIPT_PATH"/"$file" "$HOME"/"$file"
     fi
 done
